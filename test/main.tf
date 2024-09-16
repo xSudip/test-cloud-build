e9b68645263fb960b9db0bc7f2bdbf0a465b0c43
xsudip <sudip.pramanick@searce.com>
2024-09-17 00:52:51 +0530

    testing cloud build

---
+++ test/main.tf
@@ -0,0 +1,26 @@
+data "google_cloudfunctions_function" "my-function" {
+  name = "list_running_instances"
+}
+	
+output "bucket_name" {
+  value = data.google_cloudfunctions_function.my-function.id
+}
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.41.0"
+    }
+    # hashicorp = {
+    #   source = "hashicorp/time"
+    # }
+  }
+}
+
+provider "google" {
+  project = "cloudside-academy"
+  region = "us-central1"
+  zone = "us-cemtral1-a"
+}
